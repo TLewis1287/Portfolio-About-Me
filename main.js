9650b6027ed1d1601617069f4b69b924b8e26942
Tiffany <tlewis1287@gmail.com>
2021-02-15 12:51:24 -0500

    Images
    -Replaced white logo with blue logo
    -adjusted lotus image
    -adjusted floral background image
    -reposition statement

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeColor(obj) {
+    if (obj.style.color == 'rgb(242, 198, 160)') {
+        obj.style.color = 'rgb(0, 151, 167)';
+    } else {
+        obj.style.color = 'rgb(242, 198, 160)';
+    }
+}
